c5c328e116124352c33e46c47e0b57be48b11105
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-07-22 15:14:04 -0300

    criando counter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-const express = require('express')
+const express = require('express');
+const { readFileSync, writeFileSync } = require('fs');
 
-const app = express()
+const app = express();
 
-app.get('*', (req, res) => res.json({
+app.get('*', (req, res) => {
+  const file = './counter.txt';
+  const encode = 'utf-8';
+  const counter = Number(readFileSync(file, encode)) + 1;
+
+  res.json({
+    counter,
     url: req.url,
     method: req.method,
     headers: req.headers,
     params: req.params,
     query: req.query,
     body: req.body,
-}))
+  });
 
-const port = process.env.PORT || 3000
+  writeFileSync(file, counter, encode);
+});
 
-app.listen(port, () => console.log(`rodando na porta ${port}`))
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`rodando na porta ${port}`));
